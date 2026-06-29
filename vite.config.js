be619d636e982f42aabbd6332ebbc79f68f6ecae
gautam-wt-26 <gautam.prajapat@wissen.com>
2026-06-29 12:02:42 +0530

    server proxy

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://16.16.27.130:8080',
-        changeOrigin: true,
-      },
-    },
-  },
 })
